07276d0b53c55e6cf458bd2fa70359342a55e529
ForNotWork <120727683+ForNotWork@users.noreply.github.com>
2023-02-28 08:58:39 +0530

    Update Particle.cpp

--- Particle.cpp
+++ Particle.cpp
@@ -8,29 +8,35 @@ namespace code{
 
         // m_xspeed = 0.001 * (((2.0 * rand())/RAND_MAX) - 1);
         // m_yspeed = 0.001 * (((2.0 * rand())/RAND_MAX) - 1);
-
+        init();
+            }
+    Particle::~Particle(){
+        m_x=0;
+        m_y=0;
         m_direction = (2* M_PI *rand())/RAND_MAX;
         m_speed = (0.001 * rand())/RAND_MAX;
+        m_speed *= m_speed;
     }
-    Particle::~Particle(){
-
-    }
-    void Particle::update(){
+    void Particle::update(int interval){
         // m_x += m_xspeed;
         // m_y += m_yspeed;
-
-        // if(m_x <= -1.0 || m_x >= 1.0){
-        //     m_xspeed = - m_xspeed;
-        // }
-        // if(m_y <= -1.0 || m_y >= 1.0){
-        //     m_yspeed = - m_yspeed;
-        // }
-
+        
+        m_direction += interval * 0.0004;
         double xspeed = m_speed * cos(m_direction);
         double yspeed = m_speed * sin(m_direction);
 
-        m_x += xspeed;
-        m_y += yspeed;
+        m_x += xspeed * interval;
+        m_y += yspeed * interval;
+        
+        if(m_x <= -1.0 || m_x >= 1.0){
+             init();
+        }
+        if(m_y <= -1.0 || m_y >= 1.0){
+            init();
+        }
+        if(rand()<RAND_MAX/100){
+            init();
+        }
     }
     
 }
