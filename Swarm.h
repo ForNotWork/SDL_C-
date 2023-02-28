444122ebd65e8797c48eb4731e6842d991d18117
ForNotWork <120727683+ForNotWork@users.noreply.github.com>
2023-02-28 09:16:24 +0530

    Update Swarm.h

--- Swarm.h
+++ Swarm.h
@@ -7,10 +7,11 @@ namespace code{
             const static int NPARTICLES = 5000;
         private:
         Particle * m_pParticle;
+        int lastTime;
         public:
             Swarm();
             virtual ~Swarm();
-            void update();
+            void update(int elapsed);
             const Particle * const getParticles(){ return m_pParticle;};
     };
 }/*namespace code*/
