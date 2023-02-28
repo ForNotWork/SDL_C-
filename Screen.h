ed8355fe14dba2c01c4aa05e7f849ad771fddb86
ForNotWork <120727683+ForNotWork@users.noreply.github.com>
2023-02-28 09:12:51 +0530

    Update Screen.h

--- Screen.h
+++ Screen.h
@@ -14,7 +14,8 @@ namespace code
         SDL_Window *m_window;
         SDL_Renderer *m_renderer;
         SDL_Texture *m_texture;
-        Uint32 *m_buff;
+        Uint32 *m_buff1;
+        Uint32 *m_buff2;
     public:
         Screen();
         bool init();
@@ -22,7 +23,8 @@ namespace code
         void setPixel(int x, int y, Uint8 red, Uint8 green, Uint8 blue);
         bool processEvent();
         void close();
-        void clear();
+        //void clear();
+        void boxBlur();
     };
 
 }
